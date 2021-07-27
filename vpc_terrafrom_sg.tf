8dcb0feb0f563d19928406f26a351b8de68d5bd8
shivagottam <85722878+shivagottam@users.noreply.github.com>
2021-07-27 14:53:45 +0530

    Update vpc_terrafrom_sg.tf

--- vpc_terrafrom_sg.tf
+++ vpc_terrafrom_sg.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
 provider "aws" {
    access_key = "AKIAUFITGSV6DCYR5N6C"
 
